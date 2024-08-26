643f2a37cd076642192033321b8a2602b5d98d7b
strana-alynne <salockser@gmail.com>
2024-08-26 15:43:50 +0800

    frontend

---
+++ frontend/app/pages/VaccineInventory/page.js
@@ -0,0 +1,118 @@
+"use client";
+import React from "react";
+import SideBar from "@/app/components/SideBar/page";
+import {
+  Box,
+  Container,
+  Typography,
+  Stack,
+  TableContainer,
+  TableCell,
+  TableHead,
+  TableRow,
+  Table,
+  TableBody,
+  Paper,
+  Button,
+} from "@mui/material";
+import VaccinesIcon from "@mui/icons-material/Vaccines";
+
+function createData(name, calories, fat, carbs, protein, date) {
+  return { name, calories, fat, carbs, protein, date };
+}
+
+const rows = [
+  createData(
+    "0001",
+    "BCG (Bacillus-Calmette-Guerin)",
+    12,
+    50,
+    "24 hours",
+    "08/01/2024"
+  ),
+  createData("0002", "Hepatitis B", 12, 50, "24 hours", "08/01/2024"),
+  createData("0003", "Penta: DTwP-HepBHib", 12, 50, "24 hours", "08/01/2024"),
+  createData("0004", "OPV", 12, 50, "24 hours", "08/01/2024"),
+  createData(
+    "0005",
+    "MR (Measles - Rubella Vaccine)",
+    12,
+    50,
+    "24 hours",
+    "08/01/2024"
+  ),
+  createData(
+    "0006",
+    "MMR (Measles - Mumps - Rubella VAccine)",
+    12,
+    50,
+    "24 hours",
+    "08/01/2024"
+  ),
+];
+
+export default function VaccinInventory() {
+  const handleButtonClick = (id) => {
+    console.log(`Button clicked for ${id}`);
+  };
+  return (
+    <Box sx={{ display: "flex", marginTop: "50px" }}>
+      <SideBar />
+      <Container fixed>
+        <Stack spacing={4}>
+          <Stack direction="row" spacing={0.5}>
+            <VaccinesIcon sx={{ fontSize: 40 }} color="primary" />
+            <Typography variant="h2" color="primary">
+              Vaccine Inventory
+            </Typography>
+          </Stack>
+          <Box>
+            {" "}
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Vaccine ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Quantity</TableCell>
+                    <TableCell>Total</TableCell>
+                    <TableCell>Administered Interval</TableCell>
+                    <TableCell>Restock Date</TableCell>
+                    <TableCell></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell>{row.calories}</TableCell>
+                      <TableCell>{row.fat}</TableCell>
+                      <TableCell>{row.carbs}</TableCell>
+                      <TableCell>{row.protein}</TableCell>
+                      <TableCell>{row.date}</TableCell>
+                      <TableCell>
+                        {" "}
+                        <Button
+                          variant="text"
+                          color="secondary"
+                          onClick={() => handleButtonClick(row.name)}
+                        >
+                          View
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
---
+++ frontend/app/theme.js
@@ -0,0 +1,70 @@
+"use client";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import React from "react";
+
+export default function Theme({ children }) {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#0e6b58",
+        contrastText: "#f5fafb",
+      },
+      secondary: {
+        main: "#EE7423",
+        contrastText: "#f5fafb",
+      },
+      error: {
+        main: "#E94A4A",
+        contrastText: "#f5fafb",
+      },
+    },
+
+    typography: {
+      fontFamily: "Inter",
+      h1: {
+        fontSize: "3rem",
+        fontWeight: 700,
+      },
+      h2: {
+        fontSize: "2.5rem",
+        fontWeight: 700,
+      },
+      h3: {
+        fontSize: "2rem",
+        fontWeight: 700,
+      },
+      h4: {
+        fontSize: "1.75rem",
+        fontWeight: 700,
+      },
+      h5: {
+        fontSize: "1.5rem",
+        fontWeight: 700,
+      },
+      h6: {
+        fontSize: "1.25rem",
+        fontWeight: 700,
+      },
+
+      p: {
+        fontSize: "1.5rem",
+        fontWeight: 500,
+      },
+    },
+    components: {
+      MuiDrawer: {
+        styleOverrides: {
+          root: {
+            backgroundColor: "#145B50",
+          },
+        },
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
