4eb60e36c7ddf474adba418a05024bbf4243848f
strana-alynne <salockser@gmail.com>
2024-09-03 23:13:04 +0800

    Edit Add Child UI

--- frontend/app/pages/ChildRecords/AddChild/childinfo.js
+++ frontend/app/pages/ChildRecords/AddChild/childinfo.js
@@ -1,114 +1,224 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import {
-
-    Grid,
+  Grid,
   TextField,
-
+  Typography,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  InputAdornment,
+  OutlinedInput,
+  FilledInput,
 } from "@mui/material";
-
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 export default function childinfo() {
+  const purok = [
+    "Farland 1",
+    "Farland 2",
+    "Farlandville",
+    "Country Homes",
+    "New Creation",
+    "DLF",
+    "Macleod",
+    "Rasay",
+    "Sison",
+    "Back of Pepsi",
+    "Greenland",
+    "Maharlika",
+    "Pag-ibig",
+    "Philbanking",
+    "Dusnai",
+    "Sto. Rosario",
+    "Paderog",
+    "Mangrubang",
+  ];
+  const [genderVal, setGenderVal] = useState();
+  const [birthdateVal, setBirthDateVal] = useState();
+  const [purokName, setPurokName] = React.useState([]);
+
+  const handlePurokChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPurokName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  const handleGender = (event) => {
+    setGenderVal(event.target.value);
+  };
+  const handleBirthDate = (newDate) => {
+    setBirthDateVal(newDate);
+  };
   return (
     <Grid container spacing={2}>
-    <Grid item xs={6}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="First Name"
-        name="firstname"
-        autoFocus
-      />
+      {/* FIRST NAME */}
+      <Grid item xs={6}>
+        <Typography variant="p" color="darker">
+          First Name
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="First Name"
+          name="firstname"
+          autoFocus
+        />
+      </Grid>
+
+      {/* LAST NAME */}
+      <Grid item xs={6}>
+        <Typography variant="p" color="darker">
+          Last Name
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="Last Name"
+          name="lastname"
+          autoFocus
+        />
+      </Grid>
+
+      {/* AGE */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Age
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="age in months"
+          name="age"
+          autoFocus
+          type="number"
+        />
+      </Grid>
+
+      {/* GENDER */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Gender
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel id="gender-label">Gender</InputLabel>
+          <Select
+            labelId="gender-label"
+            id="gender"
+            value={genderVal}
+            label="Gender"
+            onChange={handleGender}
+          >
+            <MenuItem value="Female">Female</MenuItem>
+            <MenuItem value="Male">Male</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      {/* BIRTHDATE */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Birthdate
+        </Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Birthdate"
+            value={birthdateVal}
+            onChange={handleBirthDate}
+            renderInput={(params) => <TextField {...params} />}
+            slotProps={{
+              textField: {
+                fullWidth: true,
+                required: true,
+                variant: "filled",
+              },
+            }}
+          />
+        </LocalizationProvider>
+      </Grid>
+
+      {/* COMPLETE ADDRESS */}
+      <Grid item xs={8}>
+        <Typography variant="p" color="darker">
+          Complete Address
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="Complete Address"
+          name="address"
+          autoFocus
+        />
+      </Grid>
+
+      {/* PUROK */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Purok
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel id="purok-label">Select Purok</InputLabel>
+          <Select
+            labelId="purok-label"
+            id="purok"
+            value={purokName}
+            label="Purok"
+            onChange={handlePurokChange}
+          >
+            {purok.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+
+      {/* HEIGHT */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Height
+        </Typography>
+        <FilledInput
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="Height"
+          name="height"
+          autoFocus
+          endAdornment={<InputAdornment position="end">cm</InputAdornment>}
+        />
+      </Grid>
+
+      {/* WEIGHT */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Weight
+        </Typography>
+        <FilledInput
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="Weight"
+          name="weight"
+          autoFocus
+          endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+        />
+      </Grid>
     </Grid>
-    <Grid item xs={6}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Last Name"
-        name="lastname"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Age"
-        name="age"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Gender"
-        name="gender"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Date"
-        name="date"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={8}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Complete Address"
-        name="address"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Select a Purok"
-        name="purok"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Height"
-        name="height"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Weight"
-        name="weight"
-        autoFocus
-      />
-    </Grid>
-  </Grid>
-  )
+  );
 }
--- frontend/app/pages/ChildRecords/AddChild/motherinfo.js
+++ frontend/app/pages/ChildRecords/AddChild/motherinfo.js
@@ -1,125 +1,190 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import {
-
-    Grid,
+  Grid,
   TextField,
-
+  Typography,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
 } from "@mui/material";
 
 export default function Motherinfo() {
+  const [birthType, setBirthType] = useState();
+  const [facility, setFacility] = useState();
+  const [attending, setAttending] = useState();
+
+  const handleBirthType = (event) => {
+    setBirthType(event.target.value);
+  };
+
+  const handleFacility = (event) => {
+    setFacility(event.target.value);
+  };
+
+  const handleAttending = (event) => {
+    setAttending(event.target.value);
+  };
   return (
     <Grid container spacing={2}>
       {/* EMAIL */}
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Email Address"
-        name="email"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}>
-    <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Contact Number"
-        name="contactNumber"
-        autoFocus
-      />
-    </Grid>
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Email Address
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="e.g. name@email.com"
+          name="email"
+          autoFocus
+        />
+      </Grid>
 
-    {/* FIRST NAME */}
-    <Grid item xs={6}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="First Name"
-        name="firstname"
-        autoFocus
-      />
+      {/* CONTACT NUMBER */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Contact Number
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="e.g. 09123456789"
+          name="contactNumber"
+          autoFocus
+          type="number"
+        />
+      </Grid>
+
+      {/* FIRST NAME */}
+      <Grid item xs={5}>
+        <Typography variant="p" color="darker">
+          First Name
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="First Name"
+          name="firstname"
+          autoFocus
+        />
+      </Grid>
+
+      {/* LAST NAME */}
+      <Grid item xs={5}>
+        <Typography variant="p" color="darker">
+          Last Name
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="Last Name"
+          name="lastname"
+          autoFocus
+        />
+      </Grid>
+
+      {/* AGE */}
+      <Grid item xs={2}>
+        <Typography variant="p" color="darker">
+          Age
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="Age"
+          name="age"
+          autoFocus
+          type="number"
+        />
+      </Grid>
+
+      {/* TYPE of BIRTH */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Type of Birth
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel id="gender-label">Type of Birth</InputLabel>
+          <Select
+            labelId="gender-label"
+            id="gender"
+            value={birthType}
+            label="Gender"
+            onChange={handleBirthType}
+          >
+            <MenuItem value="Normal Delivery">Normal Delivery</MenuItem>
+            <MenuItem value="C-section">C-section</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      {/* NAME OF FACILITY */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Name of the Facility
+        </Typography>
+        <TextField
+          variant="filled"
+          size="small"
+          fullWidth
+          id="outlined-size-small"
+          label="Name of Facility"
+          name="type-facility"
+          autoFocus
+        />
+      </Grid>
+
+      {/* TPYE OF FACILITY */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Type of Facility
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel id="gender-label">Type of Facility</InputLabel>
+          <Select
+            labelId="gender-label"
+            id="gender"
+            value={facility}
+            label="Gender"
+            onChange={handleFacility}
+          >
+            <MenuItem value="Government">Government</MenuItem>
+            <MenuItem value="Private">Private</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      {/* ATTENDING */}
+      <Grid item xs={4}>
+        <Typography variant="p" color="darker">
+          Attending Personnel
+        </Typography>
+        <FormControl fullWidth variant="filled">
+          <InputLabel id="gender-label">Attending Personnel</InputLabel>
+          <Select
+            labelId="gender-label"
+            id="gender"
+            value={attending}
+            label="Gender"
+            onChange={handleAttending}
+          >
+            <MenuItem value="Doctor">Doctor</MenuItem>
+            <MenuItem value="Midwife">Midwife</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
     </Grid>
-    {/* LAST NAME */}
-    <Grid item xs={6}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Last Name"
-        name="lastname"
-        autoFocus
-      />
-    </Grid>
-    {/* AGE */}
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Age"
-        name="age"
-        autoFocus
-      />
-    </Grid>
-    {/* TYPE of BIRTH */}
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Type of Birth"
-        name="gender"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}></Grid>
-    {/* NAME OF FACILITY */}
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Name of Facility"
-        name="type-facility"
-        autoFocus
-      />
-    </Grid>
-    {/* TPYE OF FACILITY */}
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Type of Facility"
-        name="type-facility"
-        autoFocus
-      />
-    </Grid>
-    <Grid item xs={4}></Grid>
-    {/* ATTENDING */}
-    <Grid item xs={4}>
-      <TextField
-        variant="filled"
-        size="small"
-        fullWidth
-        id="outlined-size-small"
-        label="Attending Personnel"
-        name="attending-person"
-        autoFocus
-      />
-    </Grid>
-  </Grid>
-  )
+  );
 }
